75e20630b08b1f746d9b01ce14c90cd97c34730a
dadabdliu <103353468+BodaLiu@users.noreply.github.com>
2026-04-08 00:01:10 +0800

    country

---
+++ country_overrides.js
@@ -0,0 +1,26 @@
+window.countryOverrides = {
+  exact: {
+    "Taxi__2015": "Iran",
+    "Fight Club__1999": "United States",
+    "V for Vendetta__2005": "United States",
+    "One and Four__2013": "China",
+    "Duck, You Sucker__1971": "Italy",
+    "Black Dog__2024": "China",
+    "John Wick: Chapter 4__2023": "United States",
+    "Perfect Days__2023": "Japan",
+    "The Hunger Games: The Ballad of Songbirds & Snakes__2023": "United States",
+    "An Unfinished Film__2024": "China",
+    "Beyond The Wall__2022": "Iran",
+    "Autumn Sonata__1978": "Sweden",
+    "The Skeleton Key__2005": "United States",
+    "American Psycho__2000": "United States",
+    "Brooklyn__2015": "United Kingdom",
+    "Joker__2019": "United States",
+    "The Reunions__2020": "China"
+  },
+  byTitle: {
+    "A Separation": "Iran",
+    "Close-Up": "Iran",
+    "Taste of Cherry": "Iran"
+  }
+};
